68d9c59445a352f3605533e299749817343640d8
Leonel López <leonellopez647@gmail.com>
2020-10-14 16:45:30 -0600

    Corrección de error al registrar usuario

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,6 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'typeAccess' => ['required', 'number'],
         ]);
     }
 
@@ -70,6 +69,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'typeAccess' => 2,
+            'stationsSuscribed' => collect()->toArray(),
         ]);
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,13 +14,6 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-    
-        $this->middleare(function($request,$next){
-            $out = new \Symfony\Component\Console\Output\ConsoleOutput();
-            $out->writeln("Stations:aaa ".Auth::user());   
-            $this->user=Auth::user(); // here the user should exist from the session
-            return $next($request);
-        });
     }
 
     /**
